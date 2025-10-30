24549b3cf32377763ac07e425352389c7109c49c
NatalaFedorenko555 <natalafedorenko922@gmail.com>
2025-10-30 20:15:30 +0100

    server_and_client component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 
 export default function Header() {
-    return(
-
-        <div>
-            <nav>
-                <Link href={"/blog"}>Blog</Link>
-                <Link href={"/about"}>About</Link>
-                <Link href={"/blog/post"}>Post</Link>
-                <Link href={"/playground/find-play"}>Playground</Link>
-                </nav>
-        </div>
-    )
+  return (
+    <div>
+      <nav>
+        <Link href={"/blog"}>Blog</Link>
+        <Link href={"/about"}>About</Link>
+        <Link href={"/blog/post"}>Post</Link>
+        <Link href={"/playground/find-play"}>Playground</Link>
+        <Link href={"/users-client-ver"}>Users-Client</Link>
+        <Link href={"/users-server-ver"}>Users-Server</Link>
+      </nav>
+    </div>
+  );
 }
